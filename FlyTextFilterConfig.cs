e0a3195aa3d5c7b3d2f16601b70d350d28a540cc
aers <aers00@gmail.com>
2021-08-18 05:54:22 -0700

    scuffed

---
+++ FlyTextFilterConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Dalamud.Configuration;
+using Dalamud.Plugin;
+
+namespace FlyTextFilter
+{
+    public class FlyTextFilterConfig : IPluginConfiguration
+    {
+        public int Version { get; set; } = 0;
+        
+        [NonSerialized] private DalamudPluginInterface _pluginInterface;
+
+        public bool[] KindToggleArray;
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            _pluginInterface = pluginInterface;
+            KindToggleArray = new bool[52];
+            for (int i = 0; i < 52; i++)
+                KindToggleArray[i] = true;
+        }
+
+        public void Save()
+        {
+            _pluginInterface.SavePluginConfig(this);
+        }
+    }
+}
